f81961f68f5d4dc7bd04ddb4e775cc4941b8f5b6
Thiago Fernandes <iprodutora@gmail.com>
2026-01-14 22:42:50 -0300

    Fix JS compilation in production: explicitly link controllers in manifest

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,5 +2,6 @@
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
 //= link_tree ../builds
+//= link_tree ../../javascript/controllers .js
 //= link application.css
 //= link admin.css
