138697c8645af43ffb348eb713dcbbfef7e2ae93
Thiago Fernandes <iprodutora@gmail.com>
2026-01-14 22:49:51 -0300

    Fix JS error: pin sortablejs in importmap

--- config/importmap.rb
+++ config/importmap.rb
@@ -9,3 +9,4 @@ pin "swiper" # @12.0.3
 pin "tom-select", to: "https://ga.jspm.io/npm:tom-select@2.2.2/dist/js/tom-select.complete.js"
 pin "trix"
 pin "@rails/actiontext", to: "actiontext.esm.js"
+pin "sortablejs", to: "https://ga.jspm.io/npm:sortablejs@1.15.2/modular/sortable.esm.js"
